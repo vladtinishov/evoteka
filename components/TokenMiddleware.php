ddab52efb855cd3a17b5f305a0e34abb9001b0fe
Vladislav <vtinishov@gmail.com>
2024-01-09 21:57:13 +0600

    правки в мидалвар токена

--- components/TokenMiddleware.php
+++ components/TokenMiddleware.php
@@ -8,29 +8,35 @@ use Firebase\JWT\Key;
 use PHPUnit\Exception;
 use Yii;
 use yii\base\ActionFilter;
+use yii\web\Response;
 
 class TokenMiddleware extends ActionFilter
 {
     public function beforeAction($action)
     {
-        $token = Yii::$app->request->headers->get('Authorization');
+        try {
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            $token = Yii::$app->request->headers->get('Authorization');
 
-        if ($token !== null && preg_match('/^Bearer\s+(.*?)$/', $token, $matches)) {
-            $bearerToken = $matches[1];
+            if ($token !== null && preg_match('/^Bearer\s+(.*?)$/', $token, $matches)) {
+                $bearerToken = $matches[1];
 
-            if ($data = $this->isValidToken($bearerToken)) {
-                $user = User::findOne($data->user_id);
+                if ($data = $this->isValidToken($bearerToken)) {
+                    $user = User::findOne($data->user_id);
 
-                if ($user) {
-                    Yii::$app->request->setQueryParams(['user' => $user]);
-                    return true;
+                    if ($user) {
+                        Yii::$app->request->setQueryParams(['user' => $user]);
+                        return true;
+                    }
                 }
             }
-        }
 
-        Yii::$app->response->statusCode = 401;
-        Yii::$app->response->data = ['error' => 'Unauthorized'];
-        return false;
+            throw new \Exception('');
+        } catch (\Exception $e) {
+            Yii::$app->response->statusCode = 401;
+            Yii::$app->response->data = ['error' => 'Unauthorized'];
+            return false;
+        }
     }
 
     private function isValidToken($token)
