396379823182c835009dc913ea476c67839c3367
Vladislav <vtinishov@gmail.com>
2024-01-10 18:04:39 +0600

    вынес данные для входа в бд в env

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=evoteka',
-    'username' => 'postgres',
-    'password' => '',
+    'dsn' => 'pgsql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'],
+    'username' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASSWORD'],
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
